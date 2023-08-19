fe5f103e7147bb146ce65b0038222ba650f1aaaf
imvot <110433315+imvot@users.noreply.github.com>
2023-08-19 11:28:38 -0700

    Randomize color change on hover

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function generateSquares(nbSquares, squareSize) {
         square.classList.add("square");
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
-        square.addEventListener("mouseover", event => event.target.style.backgroundColor = "black");
+        square.addEventListener("mouseover", event => event.target.style.backgroundColor = getRandomRGB());
         square.addEventListener("mouseout", event => event.target.style.backgroundColor = "white");
         grid.appendChild(square);
     }
@@ -30,6 +30,10 @@ function removeChilds(element) {
 function getUserGridSize() {
     const answer = prompt("Grid Side Size");
     return +answer > 100 ? 100 : +answer
+}
+
+function getRandomRGB() {
+    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 
 generateGrid(16)
