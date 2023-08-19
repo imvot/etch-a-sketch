5b9b0c79606f1ed365f9a4e60d13e4de50aea619
imvot <110433315+imvot@users.noreply.github.com>
2023-08-19 10:09:45 -0700

    Add flexible number of side squares grid generation

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const grid = document.querySelector("#grid");
-let sideNb = 16;
-for(let i=0; i<sideNb**2; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", event => event.target.style.backgroundColor = "black");
-    square.addEventListener("mouseout", event => event.target.style.backgroundColor = "white");
-    grid.appendChild(square);
+const GRID_SIZE = 960;
+
+function generateSquares(nbSquares, squareSize) {
+    for(let i=0; i<nbSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.addEventListener("mouseover", event => event.target.style.backgroundColor = "black");
+        square.addEventListener("mouseout", event => event.target.style.backgroundColor = "white");
+        grid.appendChild(square);
+    }
 }
+
+function generateGrid(nbSideSquares) {
+    nbSideSquares = nbSideSquares > 100 ? 100 : nbSideSquares
+    removeChilds(grid)
+    const squareSize = GRID_SIZE / nbSideSquares
+    generateSquares(nbSideSquares**2, squareSize)
+}
+
+function removeChilds(element) {
+    while(element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+generateGrid(16)
+
